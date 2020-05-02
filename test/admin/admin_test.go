e08dfe96309bf2b80d46f48829bf656e803d7a51
Jason Ernst <ernstjason1@gmail.com>
2020-05-02 10:34:00 -0700

    Added test rough-ins

---
+++ test/admin/admin_test.go
@@ -0,0 +1,11 @@
+package admin
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAdmin(t *testing.T) {
+	assert.Equal(t, 1, 1)
+}
---
+++ test/auth/auth_test.go
@@ -0,0 +1,11 @@
+package auth
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAuth(t *testing.T) {
+	assert.Equal(t, 1, 1)
+}
