aa70fecdffc04ed4f386ca4c1bc61cec2cc896d3
Jason Ernst <jason@left.io>
2020-07-28 15:16:19 -0700

    needed to move blog testing code into blog package

--- blog/blog_test.go
+++ blog/blog_test.go
@@ -1,4 +1,4 @@
-package admin_test
+package blog_test
 
 import (
 	"bytes"
@@ -8,10 +8,12 @@ import (
 	"goblog/blog"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-contrib/sessions/cookie"
+
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite" // this is the db driver
@@ -32,50 +34,36 @@ func (m *Auth) IsLoggedIn(c *gin.Context) bool {
 	return args.Bool(0)
 }
 
-func TestCreatePost(t *testing.T) {
+func TestBlogWorkflow(t *testing.T) {
 	db, _ := gorm.Open("sqlite3", ":memory:")
 	db.AutoMigrate(&auth.BlogUser{})
 	db.AutoMigrate(&blog.Post{})
 	a := &Auth{}
 	admin := admin.New(db, a, "test")
+	b := blog.New(db, a, "test")
 
 	router := gin.Default()
 	store := cookie.NewStore([]byte("changelater"))
 	router.Use(sessions.Sessions("www.jasonernst.com", store))
 	router.POST("/api/v1/posts", admin.CreatePost)
+	router.GET("/api/v1/posts", b.ListPosts)
 
-	//improper content-type
-	testPost := blog.Post{
-		Title:   "Test title",
-		Content: "This is some test content",
-	}
-	jsonValue, _ := json.Marshal(testPost)
-	req, _ := http.NewRequest("POST", "/api/v1/posts", bytes.NewBuffer(jsonValue))
+	//list all posts, should be empty
+	jsonValue, _ := json.Marshal("")
+	req, _ := http.NewRequest("GET", "/api/v1/posts", bytes.NewBuffer(jsonValue))
 	w := httptest.NewRecorder()
 	router.ServeHTTP(w, req)
-	if w.Code != http.StatusUnsupportedMediaType {
-		t.Fatalf("Expected to get status %d but instead got %d\n", http.StatusUnsupportedMediaType, w.Code)
+	expected := `[]`
+	if w.Code != http.StatusOK {
+		t.Fatalf("Expected to get status %d but instead got %d\n", http.StatusOK, w.Code)
+	}
+	if w.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v", w.Body.String(), expected)
 	}
 
-	//is not admin
-	a.On("IsAdmin", mock.Anything).Return(false).Once()
-	req.Header.Add("Content-Type", "application/json")
-	w = httptest.NewRecorder()
-	router.ServeHTTP(w, req)
-	if w.Code != http.StatusUnauthorized {
-		t.Fatalf("Expected to get status %d but instead got %d\n", http.StatusUnauthorized, w.Code)
-	}
-
-	//is admin
-	a.On("IsAdmin", mock.Anything).Return(true).Once()
-	w = httptest.NewRecorder()
-	router.ServeHTTP(w, req)
-	if w.Code != http.StatusCreated {
-		t.Fatalf("Expected to get status %d but instead got %d\n", http.StatusCreated, w.Code)
-	}
-
-	//missing title
-	testPost = blog.Post{
+	//create valid post
+	testPost := blog.Post{
+		Title:   "Test title",
 		Content: "This is some test content",
 	}
 	jsonValue, _ = json.Marshal(testPost)
@@ -84,7 +72,17 @@ func TestCreatePost(t *testing.T) {
 	a.On("IsAdmin", mock.Anything).Return(true).Once()
 	w = httptest.NewRecorder()
 	router.ServeHTTP(w, req)
-	if w.Code != http.StatusBadRequest {
-		t.Fatalf("Expected to get status %d but instead got %d\n", http.StatusBadRequest, w.Code)
+	if w.Code != http.StatusCreated {
+		t.Fatalf("Expected to get status %d but instead got %d\n", http.StatusCreated, w.Code)
 	}
+
+	//list all posts, should not be empty
+	jsonValue, _ = json.Marshal("")
+	req, _ = http.NewRequest("GET", "/api/v1/posts", bytes.NewBuffer(jsonValue))
+	w = httptest.NewRecorder()
+	router.ServeHTTP(w, req)
+	if !strings.Contains(w.Body.String(), testPost.Title) {
+		t.Errorf("Expected to see a post with title: " + testPost.Title + " but didn't")
+	}
+
 }
