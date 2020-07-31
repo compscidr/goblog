6bd63eeac709096ae9db337de04f44cd72d5995a
Jason Ernst <jason@left.io>
2020-07-31 08:06:08 -0700

    removed debugging functions

--- admin/admin.go
+++ admin/admin.go
@@ -27,41 +27,6 @@ func New(db *gorm.DB, auth auth.IAuth, version string) Admin {
 	return api
 }
 
-//debug function, shows the user table
-func (a Admin) displayUserTable() {
-	var users []auth.BlogUser
-	a.db.Find(&users)
-	log.Println(users)
-}
-
-// formatRequest generates ascii representation of a request
-func formatRequest(r *http.Request) string {
-	// Create return string
-	var request []string
-	// Add the request string
-	url := fmt.Sprintf("%v %v %v", r.Method, r.URL, r.Proto)
-	request = append(request, url)
-	// Add the host
-	request = append(request, fmt.Sprintf("Host: %v", r.Host))
-	// Loop through headers
-	for name, headers := range r.Header {
-		name = strings.ToLower(name)
-		for _, h := range headers {
-			request = append(request, fmt.Sprintf("%v: %v", name, h))
-		}
-	}
-
-	// If this is a POST, add post data
-	if r.Method == "POST" {
-		r.ParseForm()
-		request = append(request, "\n")
-		request = append(request, r.Form.Encode())
-	}
-
-	// Return the request as a string
-	return strings.Join(request, "\n")
-}
-
 //////JSON API///////
 
 //CreatePost adds a post if the user has permission
