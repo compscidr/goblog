03ffcab89bcfd4b9482f82833bf1d418ac650703
Jason Ernst <ernstjason1@gmail.com>
2022-04-17 12:22:56 -0700

    rollback to sqlite driver v1.2.6 because anything v1.3.x breaks migration

--- goblog.go
+++ goblog.go
@@ -46,15 +46,20 @@ func main() {
 		db, err = gorm.Open(sqlite.Open("test.db"), &gorm.Config{
 			DisableForeignKeyConstraintWhenMigrating: true,
 		})
+		if err != nil {
+			panic("failed to open sqlite db")
+		}
+		log.Println("opened sqlite db")
 	} else {
 		user := os.Getenv("MYSQL_USER")
 		pass := os.Getenv("MYSQL_PASSWORD")
 		dbname := os.Getenv("MYSQL_DATABASE")
 		dsn := user + ":" + pass + "@tcp(db:3306)/" + dbname + "?charset=utf8mb4&parseTime=True&loc=Local"
 		db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	}
-	if err != nil {
-		panic("failed to connect database")
+		if err != nil {
+			panic("failed to connect to mysql db")
+		}
+		log.Println("connected to mysql db")
 	}
 	err = db.AutoMigrate(&auth.BlogUser{})
 	err = db.AutoMigrate(&blog.Post{})
